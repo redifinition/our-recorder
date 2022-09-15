918e40eeba2c0f4eda011e82e1ca22a3291998ca
Qiao Liang <57929780+redifinition@users.noreply.github.com>
2022-09-15 14:29:44 +0800

    feat: 更新依赖

---
+++ src/components/DataBlock.vue
@@ -0,0 +1,119 @@
+<template>
+  <div id = "DataBlock">
+    <el-card style="border-radius: 10px;width: 100%;min-width: 300px">
+      <h1 style="font-weight: lighter;text-align: left;margin-block-start: 0">在一起已经{{togetherDayNum}}天</h1>
+      <div style="display: flex;align-items:center;width: 100%;height: 100%">
+        <el-avatar class = "user-avatar" :src="avatar"></el-avatar>
+          <h1 class = 'score'>
+            {{loveScore}}/100
+          </h1>
+          <el-tooltip class="item" effect="dark" :content="toolTipContent" placement="left-start" style="margin-bottom: 10%">
+            <i class="el-icon-question"></i>
+          </el-tooltip>
+        <div style="margin-left: auto;width: 30%">
+          <el-button type="primary" icon="el-icon-edit" class="new-edit-button">
+            新增记录
+          </el-button>
+          <el-button type="primary" icon="el-icon-edit" class="new-edit-button-small">
+          </el-button>
+          <h1 class="last-record-font">2022-09-16,买了盲盒,加5分</h1>
+        </div>
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import * as moment from 'moment';
+
+export default {
+  name: "DataBlock",
+  data(){
+    return{
+      avatar: "",
+      loveScore: '',
+      toolTipContent: '该分数是截止目前小狗获得的分数哦',
+      togetherDayNum: 10
+    }
+  },
+  created() {
+    this.avatar = "https://joes-bucket.oss-cn-shanghai.aliyuncs.com/img/IMG_4288.jpg"
+    this.loveScore = 87
+    const startDate = moment('2022-08-14','YYYY-MM-DD')
+    const nowDate = moment();
+    this.togetherDayNum = nowDate.diff(startDate,'day')
+  }
+}
+</script>
+
+<style scoped>
+  .user-avatar{
+    width: 150px;
+    height: 150px;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  }
+
+  .score{
+    font-size: 3em;
+    font-weight: lighter;
+    margin-left:20px;
+  }
+
+  .new-edit-button{
+    display: block;
+  }
+  .new-edit-button-small{
+    display: none;
+  }
+
+  .last-record-font{
+    font-weight: lighter;
+  }
+
+  @media screen and (max-width: 1000px) {
+    .user-avatar{
+      width: 100px;
+      height: 100px;
+      box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    }
+    .score{
+      font-size: 2em;
+    }
+  }
+
+  @media screen and (max-width: 700px) {
+    .user-avatar{
+      width: 80px;
+      height: 80px;
+      box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    }
+    .score{
+      font-size: 1em;
+    }
+
+    .last-record-font{
+      font-size: 1em;
+      text-align: center;
+    }
+  }
+
+  @media screen and (max-width: 600px) {
+    .user-avatar{
+      width: 60px;
+      height: 60px;
+      box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    }
+    .score{
+      font-size: 1em;
+    }
+    .new-edit-button{
+      display: none;
+    }
+    .new-edit-button-small{
+      display: block;
+    }
+    .last-record-font{
+      font-size: 0.5em;
+    }
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,81 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container>
+      <el-header style="background-color: #2c3e50">Header</el-header>
+      <el-container>
+        <el-aside width="30%" style="min-width: 200px;height: 700px">
+          <div style="height: 100%">
+            <el-radio-group v-model="isCollapse" style="margin-bottom: 24px;margin-top: 24px">
+              <el-radio-button :label="false">展开</el-radio-button>
+              <el-radio-button :label="true">收起</el-radio-button>
+            </el-radio-group>
+            <el-menu default-active="1-1-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+              <el-submenu index="1">
+                <template slot="title">
+                  <i class="el-icon-pie-chart"></i>
+                  <span slot="title">我的</span>
+                </template>
+                <el-submenu index="1-1">
+                  <span slot="title">统计</span>
+                  <el-menu-item index="1-1-1">历史</el-menu-item>
+                </el-submenu>
+              </el-submenu>
+              <el-menu-item index="2">
+                <i class="el-icon-menu"></i>
+                <span slot="title">导航二</span>
+              </el-menu-item>
+              <el-menu-item index="3" disabled>
+                <i class="el-icon-document"></i>
+                <span slot="title">导航三</span>
+              </el-menu-item>
+              <el-menu-item index="4">
+                <i class="el-icon-setting"></i>
+                <span slot="title">导航四</span>
+              </el-menu-item>
+            </el-menu>
+          </div>
+        </el-aside>
+        <el-main>
+          <DataBlock></DataBlock>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import DataBlock from "@/components/DataBlock";
 
 export default {
   name: 'HomeView',
   components: {
+    DataBlock,
     HelloWorld
+  },
+  data(){
+    return{
+      isCollapse: true
+    }
+  },
+  methods:{
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    }
   }
 }
 </script>
+
+<style>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 70%;
+  margin-left: 10%;
+  min-height: 400px;
+  box-shadow: rgba(136, 165, 191, 0.48) 6px 2px 16px 0px, rgba(255, 255, 255, 0.8) -6px -2px 16px 0px;
+  border-radius: 10px;
+}
+</style>
